65f46664ea60de2fe54982c7e667886688ba0eb4
gonzaloemr <70241274+gonzaloemr@users.noreply.github.com>
2022-01-01 21:39:34 +0100

    Add files via upload

---
+++ segment_and_save.m
@@ -0,0 +1,33 @@
+function segment_and_save()
+
+folder_images= 'C:\Users\Gonzalo\Desktop\Universidad\MAIA Master\First Semester\Image Processing\Project\test\images';
+folder_mask='C:\Users\Gonzalo\Desktop\Universidad\MAIA Master\First Semester\Image Processing\Project\test\mask';
+images= fullfile(folder_images, '*.tif');
+image_files= dir(images);
+mask= fullfile(folder_mask, '*.gif');
+mask_files= dir(mask);
+num_tiles = [8 8];
+nbins = 512;
+distribution = 'uniform';
+avg_filter_size = [9 9];
+binarization_threshold = 0.01;
+areaopen_size = 100;
+morphological_op = 'majority';
+
+dir_test= "C:\Users\Gonzalo\Desktop\Universidad\MAIA Master\First Semester\Image Processing\Project\test";
+foldername = "predicted_" + datestr(now,"ddmmyy") + "_" + datestr(now,"hhmmss");
+fullpath = dir_test + "\" + foldername ;
+disp(fullpath);
+[status, msg, msgID] = mkdir(fullpath);
+
+for i=1:length(image_files)
+  name_image=image_files(i).name;
+  name_mask= mask_files(i).name;
+  image_route= fullfile(folder_images,name_image);
+  mask_route= fullfile(folder_mask, name_mask);
+  result_image= VesselSegmentation(image_route, mask_route,num_tiles,nbins,distribution,avg_filter_size, binarization_threshold,areaopen_size,morphological_op);
+  imwrite(result_image, fullfile(fullpath,append(sprintf("%d",i),".png")));
+
+end
+
+end
