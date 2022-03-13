c39ad3b3e729c1bd3082f1fa8e28034d9438acd2
manasikattel <manasikattel1@gmail.com>
2022-03-13 17:04:51 +0100

    Edit some operations for segmentation

--- VesselSegmentation.m
+++ VesselSegmentation.m
@@ -1,20 +1,23 @@
 function [im_final] = VesselSegmentation(image,mask,num_tiles,nbins,distribution,avg_filter_size, binarization_threshold,areaopen_size,morphological_op)
 
-image = imread(image);
-mask = imread(mask);
-resized_image = imresize(image, [584 565]);
-SE = strel('disk', 6);
+% Vessel Segmentation for single image
+
+% image = imread(image);
+% mask = imread(mask);
+% resized_image = imresize(image, [584 565]);
+% mask = imresize(mask, [584 565]);
+SE = strel('disk', 4);
 mask = imerode(mask, SE);
 mask = mask/255;
+resized_image= image;
 green = resized_image(:,:,2);
-gray_image = mask.*green;
+gray_image= green;
     
 double_depth_image = im2double(gray_image);
 
 
 double_depth_image = adapthisteq(double_depth_image,'numTiles',num_tiles,'nBins',nbins,'Distribution',distribution);
-im_thin_vess = MatchFilterWithGaussDerivative(double_depth_image, 1, 4, 12, double(mask), 2.3, 30);
-
+im_thin_vess = MatchFilter(double_depth_image, 1, 4, 12, double(mask), 2.3, 30);
 tetha = linspace(0,180,13);
 tetha(end) = [];
 structuring_element = strel('line',7,tetha(1));
@@ -25,21 +28,14 @@ for i= 2:numel(tetha)
     final_opening = max(final_opening, temporal_opening);
 end 
 
-smoothed_image = imreconstruct(final_opening,double_depth_image);
-
+smoothed_image= final_opening;
 average_filter = fspecial('average',avg_filter_size);
 smoothed_image2 = imfilter(final_opening, average_filter);
-
 final_image = imsubtract(smoothed_image2, smoothed_image);
 final_image = final_image.*double(mask);
-
 final_image = imbinarize(final_image, binarization_threshold);
 
 opened_image = bwareaopen(final_image, areaopen_size);
 result_image = bwmorph(opened_image,morphological_op);
-
-im_final = combine_thin_vessel(im_thin_vess,result_image);
-
-
-
+im_final= combine_two_methods(im_thin_vess, result_image);
 end 
